80f0cf977697a9129062d3d44f01fbaa6fbd1f72
devsurimlee <devsurimlee@gmail.com>
2020-02-02 23:21:34 +0900

    sd

--- algorithm/appClientModule/co/sr/algorithm/Lv4.java
+++ algorithm/appClientModule/co/sr/algorithm/Lv4.java
@@ -5,15 +5,7 @@ import java.util.Scanner;
 public class Lv4 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int x = sc.nextInt();
-		int n = sc.nextInt();
-		
-		double tmp = 1.0;
-		while (n-- > 0) {
-			tmp *= x;
-			System.out.println(n +"!");
-		}
-		System.out.println(tmp);
+
 	}
 
 }
