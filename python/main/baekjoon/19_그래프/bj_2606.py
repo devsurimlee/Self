7e43d27ee963077fdc97d2ac5e7a5ec3f855db35
srlee <devsurimlee@gmail.com>
2022-02-18 08:04:14 +0900

    2606 완료

--- python/main/baekjoon/19_그래프/bj_2606.py
+++ python/main/baekjoon/19_그래프/bj_2606.py
@@ -2,15 +2,15 @@ import sys
 
 input = sys.stdin.readline
 
-# n = int(input())
-# m = int(input())
+n = int(input())
+m = int(input())
 # n = 7
 # m = 6
 # case = [[1, 2], [2, 3], [1, 5], [5, 2], [5, 6], [4, 7]]
-
-n = 3
-m = 2
-case = [[1, 3], [2, 3]]
+#
+# n = 3
+# m = 2
+# case = [[1, 3], [2, 3]]
 arr = [[0 for i in range(n + 1)] for i in range(n + 1)]
 
 cnt = set()
@@ -18,19 +18,20 @@ def dfs(start):
     global cnt
     for i in range(1, n + 1):
         if arr[start][i] == 1:
-            arr[start][i] = 0
-            print(start, i)
+            arr[start][i] = arr[i][start] = 0
+            # print(start, i)
+            cnt.add(i)
+            cnt.add(start)
             dfs(i)
 
 
 for i in range(m):
-    # a, b = map(int, input().split())
-    a, b = case[i][0], case[i][1]       # 테스트용
-    arr[a][b] = 1
+    a, b = map(int, input().split())
+    # a, b = case[i][0], case[i][1]       # 테스트용
+    arr[a][b] = arr[b][a] = 1
 
 dfs(1) # 1부터 시작
 print(len(cnt) - 1)
-
 
 
 
