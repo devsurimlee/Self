bc5f73025c838f1824e65de353d5518699ea8805
devsurimlee <devsurimlee@gmail.com>
2022-05-01 20:19:51 +0900

    완료: 11660 / 구간합 구하기 5

--- python/main/baekjoon/16_다이나믹프로그래밍/bj_11660.py
+++ python/main/baekjoon/16_다이나믹프로그래밍/bj_11660.py
@@ -1,23 +1,24 @@
-# width, coin = map(int, input().split())
+import sys
 
-width, coin = 4, 3
-dp = [[0] * (width + 1) for i in range(width + 1)]
-case = [[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 3, 4, 5], [0, 3, 4, 5, 6], [0, 4, 5, 6, 7]]
+input = sys.stdin.readline
+width, coin = map(int, input().split())
 
-for i in range(1, width + 1):
-    # nums = list(map(int, input().split()))
-    nums = case[i]
-    for j in range(1, width + 1):
-        dp[i][j] = nums[j]
+dp = [list(map(int, input().split())) for _ in range(width)]
 
-for x in range(1):
-    # x1, y1, x2, y2 = list(map(int, input().split()))
+for i in range(width):
+    for j in range(width):
+        if j == 0:
+            continue
+        dp[i][j] += dp[i][j - 1]
+
+for _ in range(coin):
+    x1, y1, x2, y2 = list(map(int, input().split()))
     sum = 0
-    x1, y1, x2, y2 = 2, 2, 3, 4
 
-    for i in range(x1, x2 + 1):
-        for j in range(y1, y2 + 1):
-            sum += dp[i][j]
+    for z in range(x1 - 1, x2):
+        sum += dp[z][y2 - 1]
+        if y1 != 1:
+            sum -= dp[z][y1 - 2]
 
     print(sum)
 
